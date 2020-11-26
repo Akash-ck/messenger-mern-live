8b8e0be68fa019232d3fabf95ee5dffb772b34d2
akash-ck <64592857+Akash-ck@users.noreply.github.com>
2020-11-26 21:38:20 +0530

    make it better

---
+++ messageModel.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const messageSchema = mongoose.Schema({
+
+    username: String,
+    message: String,
+    timestamp:String
+
+})
+
+export default mongoose.model('messages', messageSchema)
